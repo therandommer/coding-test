d70da7faae1c62abbbb79b0e939847b4c47e06c5
Isabella <bdc190799@gmail.com>
2023-09-05 17:04:18 +0100

    Added test questions
    Added test questions using the correct object storage layout. To be populated with actual questions later.

---
+++ starter/js/questions.js
@@ -0,0 +1,17 @@
+let questions = [
+    {
+        "title": "Test question title",
+        "choices": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        "answer": "Answer 1"
+    },
+    {
+        "title": "Test question title 2",
+        "choices": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        "answer": "Answer 2"
+    },
+    {
+        "title": "Test question title 3",
+        "choices": ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
+        "answer": "Answer 3"
+    }
+]
