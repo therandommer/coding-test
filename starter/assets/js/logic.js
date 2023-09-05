70380fb8578434c1f0da75c262c3df06ee80fca1
Isabella <bdc190799@gmail.com>
2023-09-05 17:38:32 +0100

    Added start, end and timer logic
    Added functionality to call the start, end and timer parts of the quiz. The page currently will hide the starter text and will set a timer to count down at the correct rate 1/second.

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -2,7 +2,7 @@
 let currentQuestionIndex = 0; //used to reference the question array.
 let timePerQuestion = 20; //modify this to make the game more or less difficult.
 let time = questions.length * timePerQuestion; //used for the countdown function
-let timerID;
+let timerID = 0;
 
 //---HTML Elements---
 let questionsElement = document.getElementById("questions");
@@ -28,20 +28,39 @@ function questionClick()
 
 }
 
-function startQuiz()
-{
-    alert("Quiz started");
-}
-
 function quizEnd()
 {
+    clearInterval(timerID); //stops countdown
 
+    let endScreenElement = document.getElementById("end-screen");
+    endScreenElement.removeAttribute("class"); //display the end screen element
+    
+    let finalScoreElement = document.getElementById("final-score");
+    finalScoreElement.textContent = time; //final score equal to time 
+    questionsElement.setAttribute("class", "hide"); //hides the questions element
 }
 
 function timer()
 {
-
+    time --;
+    timerElement.textContent = time;
+    if(time <= 0)
+    {
+        quizEnd();
+    }
 }
+
+function startQuiz()
+{
+    let startScreenElement = document.getElementById("start-screen");
+    startScreenElement.setAttribute("class", "hide"); //removes initial screen to make way for future questions
+    questionsElement.removeAttribute("class");
+
+    timerID = setInterval(timer, 1000); //will call the timer function once every 1000ms or 1s to countdown
+}
+
+
+
 
 function saveHighScore()
 {
@@ -53,4 +72,7 @@ function checkForEnter(event)
 
 }
 
-startButton.addEventListener("click", startQuiz);
+//---Enent initialisers---
+startButton.addEventListener("click", startQuiz); //starting the quiz
+submitButton.addEventListener("click", saveHighScore);//finishing the game and submitting to high score
+initialElement.addEventListener("keyup", checkForEnter)//submitting initials on enter press
