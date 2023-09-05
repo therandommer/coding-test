be5e8b13517a5839111a97cfdfd8e54cfccb0025
Isabella <bdc190799@gmail.com>
2023-09-05 17:14:34 +0100

    Object references
    Created a lot of the references to HTML elements required for the future logic to work.

---
+++ starter/assets/js/logic.js
@@ -0,0 +1,18 @@
+//---quiz state variables---
+let currentQuestionIndex = 0; //used to reference the question array.
+let timePerQuestion = 20; //modify this to make the game more or less difficult.
+let time = questions.length * timePerQuestion; //used for the countdown function
+let timerID;
+
+//---HTML Elements---
+let questionsElement = document.getElementById("questions");
+let timerElement = document.getElementById("time");
+let choicesElement = document.getElementById("choices");
+let submitButton = document.getElementById("submit");
+let startButton = document.getElementById("start");
+let initialElement = document.getElementById("initials");
+let feedbackElement = document.getElementById("feedback");
+
+//---Audio Element---
+let correctSound = new Audio("assets/sfx/correct.wav");
+let wrongSound = new Audio("assets/sfx/incorrect.wav");
