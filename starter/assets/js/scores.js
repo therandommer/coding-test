c04a064f572de7ffbcdfa4c2f707099baa046a30
Isabella <bdc190799@gmail.com>
2023-09-05 19:15:34 +0100

    Print and clear scores
    Added functionality to create a high score and render it to the browser. Also added the ability to delete the high score list. This function will then reload the page to show you the results of the button press immediately.

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -1,11 +1,26 @@
 function printHighScore()
 {
+    let newHighScore = JSON.parse(localStorage.getItem("highscores")) || []; //load highscores from the local memory
 
+    newHighScore.sort(function(firstScore, secondScore) //sorts the scores by highest to lowest
+    {
+        return secondScore.score - firstScore.score; 
+    });
+
+    newHighScore.forEach(function(score){ //creating list for high score table
+        let newLi = document.createElement("li");
+        newLi.textContent = `${score.initials} - ${score.score}`;
+
+        let olElement = document.getElementById("highscores"); //appends each li element to the ol as a new child
+        olElement.appendChild(newLi);
+    });
 }
 
 function clearHighScore()
 {
-    console.log("Clearing high score");
+    localStorage.removeItem("highscores");
+    printHighScore();//updates the highscore table
+    location.reload(); //reloads the browser to show the updated table
 }
 
 let clearButton = document.getElementById("clear"); //reference to the clear button for future use. Referenced in highscores.html
